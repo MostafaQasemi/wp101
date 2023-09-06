ec9da8aed921a7eb788c46138f073772bc239cf2
MostafaQasemi <77499474+MostafaQasemi@users.noreply.github.com>
2023-09-06 19:22:40 +0330

    initial customization theme

--- wp-content/themes/awesometheme/functions.php
+++ wp-content/themes/awesometheme/functions.php
@@ -3,9 +3,17 @@
 function awesome_script_enqueue(){
     wp_enqueue_style('customstyle',get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0.', 'all');
 
+    wp_enqueue_script('customscript',get_template_directory_uri().'/js/awesome.js',array(), '1.0.0', true);
 
 }
 
-add_action('wp_enqueue_script','awesome_script_enqueue');
+add_action('wp_enqueue_scripts','awesome_script_enqueue');
 
+function awesome_theme_setup(){
+    add_theme_support('menu');
+    add_theme_support('widgets');
+    
+}
+
+add_action('init','awesome_theme_setup');
 ?>
